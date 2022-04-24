40518490e746b97676cab56c431c670b4b4f38ea
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-04-24 17:00:44 +0200

    ...

--- MoveToFrontTesting/text_files/getfiles.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-for i in range(10000):
-    try:
-        p = subprocess.run(f"wget https://www.gutenberg.org/ebooks/{i}.txt.utf-8")
-    except:
-        print(f"failed: {i}")
-
